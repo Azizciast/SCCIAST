73473a6723827d87d9314cb4f368eb031e6def40
Azizciast <wanabdulaziz@ciast.edu.my>
2024-03-20 02:02:27 +0800

    add file inc

--- tempahan.php
+++ tempahan.php
@@ -28,47 +28,18 @@ if (isset($_POST["submit"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tempahan</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="bootstrap-icons-1.11.3/font/bootstrap-icons.min.css">
+    <?php require_once('inc/link.php'); ?>
+
 </head>
 
 <body>
-<i class="bi bi-pencil-square" style="font-size: 3rem;"></i>
-<nav class="navbar navbar-dark bg-dark fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">System bookings court ciast</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">System bookings court ciast</h5>
-        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="dashboard.php">Home</a>
-          </li>
 
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="tempahan.php">Fasiliti</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="logout.php">Log out</a>
-          </li>
-      
-      </div>
-    </div>
-  </div>
-</nav>
-
+    <?php require_once('inc/navbar.php'); ?><br><br>
+    
     <h1>Tempahan gelanggang</h1>
-    <br>
-    <br>
-   
-    <div class="row row-cols-1 row-cols-md-3 g-4">
+
+   <br>
+    <div class="row row-cols-3 row-cols-md-4 g-3">
         <div class="col">
             <div class="card h-100">
                 <img src="https://assets.hmetro.com.my/images/articles/fts_cropped_1505627891.jpg" class="card-img-top" alt="...">
@@ -143,18 +114,18 @@ if (isset($_POST["submit"])) {
                                 <input type="time" name="masa_berakhir" id="akhir">
                             </li>
                         </ul>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button class="btn btn-primary" type="submit" name="submit">Bookings now</button>
+                        </div>
                     </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" type="submit" name="submit">Bookings now</button>
                 </div>
             </div>
         </div>
     </div>
     <br><br>
     <a href="list_tempahan.php" style="text-decoration: none;">Senarai Tempahan</a>
-    <script src="js/bootstrap.min.js"></script>
+    <?php require_once('inc/script.php'); ?>
 </body>
 
 </html>
